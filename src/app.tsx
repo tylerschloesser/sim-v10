30230920f1e98a790dc8fc49e2b59bad8daa2c82
Tyler Schloesser <tylerschloesser@gmail.com>
2024-09-07 08:06:24 -0700

    add interval

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,7 @@
+import { useEffect } from 'react'
 import { useImmer } from 'use-immer'
+
+const TICK_INTERVAL: number = 1000
 
 interface State {
   tick: number
@@ -6,6 +9,16 @@ interface State {
 
 export function App() {
   const [state, setState] = useImmer<State>({ tick: 0 })
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setState((draft) => {
+        draft.tick += 1
+      })
+    }, TICK_INTERVAL)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
   return (
     <div>
       <div>Tick: {state.tick.toString()}</div>
