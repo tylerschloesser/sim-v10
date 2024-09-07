eb16ad035d72a77a52da4d544565430fd4c81ad3
Tyler Schloesser <tylerschloesser@gmail.com>
2024-09-07 09:05:18 -0700

    fix drag and drop

--- src/app.tsx
+++ src/app.tsx
@@ -1,28 +1,39 @@
 import clsx from 'clsx'
-import { Fragment, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import shortId from 'short-uuid'
+import invariant from 'tiny-invariant'
 import { Updater, useImmer } from 'use-immer'
+import { z } from 'zod'
 
 const TICK_INTERVAL: number = 1000
 
-interface Item {
-  id: string
-  name: string
-}
+const ItemLocation = z.enum(['Queue', 'Available'])
+type ItemLocation = z.infer<typeof ItemLocation>
+
+const Item = z.strictObject({
+  id: z.string(),
+  name: z.string(),
+  location: ItemLocation,
+})
+type Item = z.infer<typeof Item>
 
 interface State {
   tick: number
-  queue: Item[]
-  available: Item[]
-  drag: boolean
+  drag: string | null
+  items: Item[]
 }
 
 export function App() {
   const [state, setState] = useImmer<State>({
     tick: 0,
-    queue: [],
-    available: [{ id: shortId.generate(), name: 'stone' }],
-    drag: false,
+    items: [
+      {
+        id: shortId.generate(),
+        name: 'stone',
+        location: ItemLocation.enum.Available,
+      },
+    ],
+    drag: null,
   })
   useEffect(() => {
     const interval = setInterval(() => {
@@ -38,19 +49,21 @@ export function App() {
     <div className="flex flex-col p-2 gap-2">
       <div>Tick: {state.tick.toString()}</div>
       <div className="flex gap-2">
-        <div className="flex-1">
+        <div className="flex-1 flex flex-col gap-2">
           <h2>Queue</h2>
-          <Queue state={state} />
+          <ItemList
+            location={ItemLocation.enum.Queue}
+            state={state}
+            setState={setState}
+          />
         </div>
         <div className="flex-1 flex flex-col gap-2">
           <h2>Available</h2>
-          <div>
-            {state.available.map((item) => (
-              <Fragment key={item.id}>
-                <Card item={item} setState={setState} />
-              </Fragment>
-            ))}
-          </div>
+          <ItemList
+            location={ItemLocation.enum.Available}
+            state={state}
+            setState={setState}
+          />
         </div>
       </div>
     </div>
@@ -66,18 +79,15 @@ function Card({ item, setState }: CardProps) {
   const [drag, setDrag] = useState(false)
   useEffect(() => {
     setState((draft) => {
-      draft.drag = drag
+      draft.drag = drag ? item.id : null
     })
-  }, [drag])
-
+  }, [drag, item.id])
   return (
     <div
+      role="button"
       draggable
       onDragStart={(ev) => {
-        ev.dataTransfer.setData(
-          'text/plain',
-          JSON.stringify(item),
-        )
+        ev.dataTransfer.setData('text/plain', item.id)
         setDrag(true)
       }}
       onDragEnd={(ev) => {
@@ -96,16 +106,29 @@ function Card({ item, setState }: CardProps) {
   )
 }
 
-interface QueueProps {
+interface ItemListProps {
+  location: ItemLocation
   state: State
+  setState: Updater<State>
 }
 
-function Queue({ state }: QueueProps) {
+function ItemList({
+  location,
+  state,
+  setState,
+}: ItemListProps) {
   return (
     <div
       onDrop={(ev) => {
         ev.preventDefault()
-        console.log(ev)
+        const itemId = ev.dataTransfer.getData('text/plain')
+        setState((draft) => {
+          const item = draft.items.find(
+            ({ id }) => id === itemId,
+          )
+          invariant(item)
+          item.location = location
+        })
       }}
       onDragOver={(ev) => {
         ev.preventDefault()
@@ -116,6 +139,16 @@ function Queue({ state }: QueueProps) {
         state.drag &&
           'border-dashed border border-gray-400',
       )}
-    ></div>
+    >
+      {state.items
+        .filter((item) => item.location === location)
+        .map((item) => (
+          <Card
+            key={item.id}
+            item={item}
+            setState={setState}
+          />
+        ))}
+    </div>
   )
 }
