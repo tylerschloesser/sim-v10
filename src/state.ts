cfadd4e707667777af9991b8a0fc660982d41147
Tyler Schloesser <tylerschloesser@gmail.com>
2024-10-14 17:24:57 -0700

    add action

--- src/state.ts
+++ src/state.ts
@@ -6,6 +6,23 @@ export type ItemType = z.infer<typeof ItemType>
 export const Inventory = z.record(ItemType, z.number())
 export type Inventory = z.infer<typeof Inventory>
 
+export const ActionType = z.enum(['Mine'])
+export type ActionType = z.infer<typeof ActionType>
+
+export const MineAction = z.strictObject({
+  type: z.literal(ActionType.enum.Mine),
+  item: z.union([
+    z.literal(ItemType.enum.Coal),
+    z.literal(ItemType.enum.Stone),
+  ]),
+})
+type MineAction = z.infer<typeof MineAction>
+
+export const Action = z.discriminatedUnion('type', [
+  MineAction,
+])
+export type Action = z.infer<typeof Action>
+
 export const State = z.strictObject({
   tick: z.number().nonnegative(),
   inventory: Inventory,
