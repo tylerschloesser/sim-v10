ba3d192289ed752fc926686ec0575914e917f9e8
Tyler Schloesser <tylerschloesser@gmail.com>
2024-10-18 20:02:36 -0700

    refactor robot

--- src/app.tsx
+++ src/app.tsx
@@ -177,25 +177,36 @@ export function App() {
                 }
               />
               {Object.values(state.robots).map((robot) => (
-                <div key={robot.id} className="flex gap-2">
-                  <div>{robot.name}</div>
-                  <RobotDialog
-                    robotId={robot.id}
-                    trigger={<div>Edit</div>}
-                  />
-                  <div
-                    onClick={() => {
-                      if (window.confirm('Are you sure?')) {
-                        setState((draft) => {
-                          invariant(draft.robots[robot.id])
-                          delete draft.robots[robot.id]
-                          // prettier-ignore
-                          draft.inventory[ItemType.enum.Robot] = (draft.inventory[ItemType.enum.Robot] ?? 0) + 1
-                        })
-                      }
-                    }}
-                  >
-                    Delete
+                <div
+                  key={robot.id}
+                  className="flex flex-col gap-2 p-2 border"
+                >
+                  <div className="flex gap-2 justify-between">
+                    <div>{robot.name}</div>
+                    <div className="flex gap-2">
+                      <RobotDialog
+                        robotId={robot.id}
+                        trigger={<div>Edit</div>}
+                      />
+                      <div
+                        onClick={() => {
+                          if (
+                            window.confirm('Are you sure?')
+                          ) {
+                            setState((draft) => {
+                              invariant(
+                                draft.robots[robot.id],
+                              )
+                              delete draft.robots[robot.id]
+                              // prettier-ignore
+                              draft.inventory[ItemType.enum.Robot] = (draft.inventory[ItemType.enum.Robot] ?? 0) + 1
+                            })
+                          }
+                        }}
+                      >
+                        Delete
+                      </div>
+                    </div>
                   </div>
                 </div>
               ))}
